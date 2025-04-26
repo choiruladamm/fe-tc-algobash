2b8aa3ee784d53a78adb31d77efdcdaa5e35e35d
choiruladamm <choiruladamm@gmail.com>
2025-04-26 10:37:38 +0700

    feat: case matrix difference
    - calculate absolute difference

--- matrix-difference/index.ts
+++ matrix-difference/index.ts
@@ -1,1 +1,29 @@
-// starter repo
+// matrix difference
+
+function solution(a: number[][]): number {
+  const n: number = a.length;
+  let leftToRight = 0;
+  let rightToLeft = 0;
+
+  for (let i = 0; i < n; i++) {
+    leftToRight += a[i][i];
+    rightToLeft += a[i][n - i - 1];
+  }
+
+  return Math.abs(leftToRight - rightToLeft);
+}
+
+const data = [
+  [3, 1, 2],
+  [5, 6, 4],
+  [7, 9, 8],
+];
+
+const data1 = [
+  [10, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.log(solution(data));
+console.log(solution(data1));
